14cd4341d430c62748eadf324d64e260d14292dd
BRid37 <55640145+BRid37@users.noreply.github.com>
2025-06-17 12:23:06 -0500

    Update sensor.py using GPT

--- custom_components/iqua_water_softener/sensor.py
+++ custom_components/iqua_water_softener/sensor.py
@@ -1,285 +1,180 @@
-from abc import ABC, abstractmethod
-from datetime import datetime, timedelta
 import logging
+from datetime import datetime, timedelta
 from typing import Optional
 
-from homeassistant.core import callback
-from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator,
-    UpdateFailed,
-    CoordinatorEntity,
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.update_coordinator import (  
+    CoordinatorEntity,  
+    DataUpdateCoordinator,  
+    UpdateFailed
+)
+from homeassistant.components.sensor import (  
+    SensorEntity,  
+    SensorEntityDescription,  
+    SensorDeviceClass,  
+    SensorStateClass
+)
+from homeassistant.const import PERCENTAGE, UnitOfVolume
+from homeassistant.config_entries import ConfigEntry
+
+from .const import (  
+    DOMAIN,  
+    CONF_USERNAME,  
+    CONF_PASSWORD,  
+    CONF_DEVICE_SERIAL_NUMBER,  
+    VOLUME_FLOW_RATE_LITERS_PER_MINUTE,  
+    VOLUME_FLOW_RATE_GALLONS_PER_MINUTE
 )
 
-from iqua_softener import (
-    IquaSoftener,
-    IquaSoftenerData,
-    IquaSoftenerVolumeUnit,
-    IquaSoftenerException,
-)
-
-from homeassistant import config_entries, core
-from homeassistant.components.sensor import (
-    SensorEntity,
-    SensorDeviceClass,
-    SensorStateClass,
-    SensorEntityDescription,
-)
-from homeassistant.const import PERCENTAGE
-from homeassistant.const import UnitOfVolume
-
-from .const import (
-    DOMAIN,
-    CONF_USERNAME,
-    CONF_PASSWORD,
-    CONF_DEVICE_SERIAL_NUMBER,
-    VOLUME_FLOW_RATE_LITERS_PER_MINUTE,
-    VOLUME_FLOW_RATE_GALLONS_PER_MINUTE,
+from iqua_softener import (  
+    IquaSoftener,  
+    IquaSoftenerData,  
+    IquaSoftenerVolumeUnit,  
+    IquaSoftenerException
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 UPDATE_INTERVAL = timedelta(minutes=15)
 
+PLATFORMS = ["sensor"]
 
-async def async_setup_entry(
-    hass: core.HomeAssistant,
-    config_entry: config_entries.ConfigEntry,
-    async_add_entities,
-):
-    config = hass.data[DOMAIN][config_entry.entry_id]
-    if config_entry.options:
-        config.update(config_entry.options)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    config = hass.data[DOMAIN][entry.entry_id]
+    if entry.options:
+        config.update(entry.options)
+
     device_serial_number = config[CONF_DEVICE_SERIAL_NUMBER]
     coordinator = IquaSoftenerCoordinator(
         hass,
         IquaSoftener(
-            config[CONF_USERNAME], config[CONF_PASSWORD], device_serial_number
-        ),
+            config[CONF_USERNAME],
+            config[CONF_PASSWORD],
+            device_serial_number
+        )
     )
     await coordinator.async_config_entry_first_refresh()
-    sensors = [
-        clz(coordinator, device_serial_number, entity_description)
-        for clz, entity_description in (
-            (
-                IquaSoftenerStateSensor,
-                SensorEntityDescription(key="State", name="State"),
-            ),
-            (
-                IquaSoftenerDeviceDateTimeSensor,
-                SensorEntityDescription(
-                    key="DATE_TIME",
-                    name="Date/time",
-                    icon="mdi:clock",
-                ),
-            ),
-            (
-                IquaSoftenerLastRegenerationSensor,
-                SensorEntityDescription(
-                    key="LAST_REGENERATION",
-                    name="Last regeneration",
-                    device_class=SensorDeviceClass.TIMESTAMP,
-                ),
-            ),
-            (
-                IquaSoftenerOutOfSaltEstimatedDaySensor,
-                SensorEntityDescription(
-                    key="OUT_OF_SALT_ESTIMATED_DAY",
-                    name="Out of salt estimated day",
-                    device_class=SensorDeviceClass.TIMESTAMP,
-                ),
-            ),
-            (
-                IquaSoftenerSaltLevelSensor,
-                SensorEntityDescription(
-                    key="SALT_LEVEL",
-                    name="Salt level",
-                    state_class=SensorStateClass.MEASUREMENT,
-                    native_unit_of_measurement=PERCENTAGE,
-                ),
-            ),
-            (
-                IquaSoftenerAvailableWaterSensor,
-                SensorEntityDescription(
-                    key="AVAILABLE_WATER",
-                    name="Available water",
-                    state_class=SensorStateClass.TOTAL,
-                    device_class=SensorDeviceClass.WATER,
-                    icon="mdi:water",
-                ),
-            ),
-            (
-                IquaSoftenerWaterCurrentFlowSensor,
-                SensorEntityDescription(
-                    key="WATER_CURRENT_FLOW",
-                    name="Water current flow",
-                    state_class=SensorStateClass.MEASUREMENT,
-                    icon="mdi:water-pump",
-                ),
-            ),
-            (
-                IquaSoftenerWaterUsageTodaySensor,
-                SensorEntityDescription(
-                    key="WATER_USAGE_TODAY",
-                    name="Today water usage",
-                    state_class=SensorStateClass.TOTAL_INCREASING,
-                    device_class=SensorDeviceClass.WATER,
-                    icon="mdi:water-minus",
-                ),
-            ),
-            (
-                IquaSoftenerWaterUsageDailyAverageSensor,
-                SensorEntityDescription(
-                    key="WATER_USAGE_DAILY_AVERAGE",
-                    name="Water usage daily average",
-                    state_class=SensorStateClass.TOTAL_INCREASING,
-                    device_class=SensorDeviceClass.WATER,
-                ),
-            ),
-        )
-    ]
-    async_add_entities(sensors)
 
+    SENSOR_TYPES = [
+        (IquaSoftenerStateSensor, SensorEntityDescription(key="State", name="State")),
+        (IquaSoftenerDeviceDateTimeSensor, SensorEntityDescription(key="DATE_TIME", name="Date/time", icon="mdi:clock")),
+        (IquaSoftenerLastRegenerationSensor, SensorEntityDescription(key="LAST_REGENERATION", name="Last regeneration", device_class=SensorDeviceClass.TIMESTAMP)),
+        (IquaSoftenerOutOfSaltEstimatedDaySensor, SensorEntityDescription(key="OUT_OF_SALT_ESTIMATED_DAY", name="Out of salt estimated day", device_class=SensorDeviceClass.TIMESTAMP)),
+        (IquaSoftenerSaltLevelSensor, SensorEntityDescription(key="SALT_LEVEL", name="Salt level", state_class=SensorStateClass.MEASUREMENT, native_unit_of_measurement=PERCENTAGE)),
+        (IquaSoftenerAvailableWaterSensor, SensorEntityDescription(key="AVAILABLE_WATER", name="Available water", state_class=SensorStateClass.TOTAL, device_class=SensorDeviceClass.WATER, icon="mdi:water")),
+        (IquaSoftenerWaterCurrentFlowSensor, SensorEntityDescription(key="WATER_CURRENT_FLOW", name="Water current flow", state_class=SensorStateClass.MEASUREMENT, icon="mdi:water-pump")),
+        (IquaSoftenerWaterUsageTodaySensor, SensorEntityDescription(key="WATER_USAGE_TODAY", name="Today water usage", state_class=SensorStateClass.TOTAL_INCREASING, device_class=SensorDeviceClass.WATER, icon="mdi:water-minus")),
+        (IquaSoftenerWaterUsageDailyAverageSensor, SensorEntityDescription(key="WATER_USAGE_DAILY_AVERAGE", name="Water usage daily average", state_class=SensorStateClass.MEASUREMENT, device_class=SensorDeviceClass.WATER)),
+    ]
+
+    async_add_entities([
+        sensor_cls(coordinator, device_serial_number, description)
+        for sensor_cls, description in SENSOR_TYPES
+    ])
 
 class IquaSoftenerCoordinator(DataUpdateCoordinator):
-    def __init__(self, hass: core.HomeAssistant, iqua_softener: IquaSoftener):
+    def __init__(self, hass: HomeAssistant, softener: IquaSoftener):
         super().__init__(
             hass,
             _LOGGER,
             name="Iqua Softener",
-            update_interval=UPDATE_INTERVAL,
+            update_interval=UPDATE_INTERVAL
         )
-        self._iqua_softener = iqua_softener
+        self.softener = softener
 
     async def _async_update_data(self) -> IquaSoftenerData:
         try:
-            return await self.hass.async_add_executor_job(
-                lambda: self._iqua_softener.get_data()
-            )
+            return await self.hass.async_add_executor_job(self.softener.get_data)
         except IquaSoftenerException as err:
-            raise UpdateFailed(f"Get data failed: {err}")
+            raise UpdateFailed(f"Failed to fetch IQua softener data: {err}")
 
-
-class IquaSoftenerSensor(SensorEntity, CoordinatorEntity, ABC):
-    def __init__(
-        self,
-        coordinator: IquaSoftenerCoordinator,
-        device_serial_number: str,
-        entity_description: SensorEntityDescription = None,
-    ):
+class IquaSoftenerSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator: IquaSoftenerCoordinator, serial: str, description: SensorEntityDescription):
         super().__init__(coordinator)
-        self._attr_unique_id = (
-            f"{device_serial_number}_{entity_description.key}".lower()
-        )
-
-        if entity_description is not None:
-            self.entity_description = entity_description
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        self.update(self.coordinator.data)
-        self.async_write_ha_state()
-
-    @abstractmethod
-    def update(self, data: IquaSoftenerData):
-        ...
-
+        self.entity_description = description
+        self._attr_unique_id = f"{serial}_{description.key}".lower()
 
 class IquaSoftenerStateSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = str(data.state.value)
-
+    @property
+    def native_value(self):
+        return str(self.coordinator.data.state.value)
 
 class IquaSoftenerDeviceDateTimeSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = data.device_date_time.strftime("%Y-%m-%d %H:%M:%S")
-
+    @property
+    def native_value(self):
+        return self.coordinator.data.device_date_time.strftime("%Y-%m-%d %H:%M:%S")
 
 class IquaSoftenerLastRegenerationSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = (
-            datetime.now(data.device_date_time.tzinfo)
-            - timedelta(days=data.days_since_last_regeneration)
-        ).replace(hour=0, minute=0, second=0)
-
+    @property
+    def native_value(self):
+        dt = self.coordinator.data.device_date_time
+        return (datetime.now(dt.tzinfo) - timedelta(days=self.coordinator.data.days_since_last_regeneration)).replace(hour=0, minute=0, second=0)
 
 class IquaSoftenerOutOfSaltEstimatedDaySensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = (
-            datetime.now(data.device_date_time.tzinfo)
-            + timedelta(days=data.out_of_salt_estimated_days)
-        ).replace(hour=0, minute=0, second=0)
-
+    @property
+    def native_value(self):
+        dt = self.coordinator.data.device_date_time
+        return (datetime.now(dt.tzinfo) + timedelta(days=self.coordinator.data.out_of_salt_estimated_days)).replace(hour=0, minute=0, second=0)
 
 class IquaSoftenerSaltLevelSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = data.salt_level_percent
+    @property
+    def native_value(self):
+        return self.coordinator.data.salt_level_percent
 
     @property
     def icon(self) -> Optional[str]:
-        if self._attr_native_value is not None:
-            if self._attr_native_value > 75:
-                return "mdi:signal-cellular-3"
-            elif self._attr_native_value > 50:
-                return "mdi:signal-cellular-2"
-            elif self._attr_native_value > 25:
-                return "mdi:signal-cellular-1"
-            elif self._attr_native_value > 5:
-                return "mdi:signal-cellular-outline"
-            return "mdi:signal-off"
-        else:
+        value = self.native_value
+        if value is None:
             return "mdi:signal"
-
+        if value > 75:
+            return "mdi:signal-cellular-3"
+        elif value > 50:
+            return "mdi:signal-cellular-2"
+        elif value > 25:
+            return "mdi:signal-cellular-1"
+        elif value > 5:
+            return "mdi:signal-cellular-outline"
+        return "mdi:signal-off"
 
 class IquaSoftenerAvailableWaterSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = data.total_water_available / (
-            1000
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else 1
-        )
-        self._attr_native_unit_of_measurement = (
-            UnitOfVolume.CUBIC_METERS
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else UnitOfVolume.GALLONS
-        )
-        self._attr_last_reset = datetime.now(data.device_date_time.tzinfo) - timedelta(
-            days=data.days_since_last_regeneration
-        )
+    @property
+    def native_value(self):
+        value = self.coordinator.data.total_water_available
+        return value / 1000 if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else value
 
+    @property
+    def native_unit_of_measurement(self):
+        return UnitOfVolume.CUBIC_METERS if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else UnitOfVolume.GALLONS
+
+    @property
+    def last_reset(self):
+        return datetime.now(self.coordinator.data.device_date_time.tzinfo) - timedelta(days=self.coordinator.data.days_since_last_regeneration)
 
 class IquaSoftenerWaterCurrentFlowSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = data.current_water_flow
-        self._attr_native_unit_of_measurement = (
-            VOLUME_FLOW_RATE_LITERS_PER_MINUTE
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else VOLUME_FLOW_RATE_GALLONS_PER_MINUTE
-        )
+    @property
+    def native_value(self):
+        return self.coordinator.data.current_water_flow
 
+    @property
+    def native_unit_of_measurement(self):
+        return VOLUME_FLOW_RATE_LITERS_PER_MINUTE if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else VOLUME_FLOW_RATE_GALLONS_PER_MINUTE
 
 class IquaSoftenerWaterUsageTodaySensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = data.today_use / (
-            1000
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else 1
-        )
-        self._attr_native_unit_of_measurement = (
-            UnitOfVolume.CUBIC_METERS
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else UnitOfVolume.GALLONS
-        )
+    @property
+    def native_value(self):
+        value = self.coordinator.data.today_use
+        return value / 1000 if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else value
 
+    @property
+    def native_unit_of_measurement(self):
+        return UnitOfVolume.CUBIC_METERS if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else UnitOfVolume.GALLONS
 
 class IquaSoftenerWaterUsageDailyAverageSensor(IquaSoftenerSensor):
-    def update(self, data: IquaSoftenerData):
-        self._attr_native_value = data.average_daily_use / (
-            1000
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else 1
-        )
-        self._attr_native_unit_of_measurement = (
-            UnitOfVolume.CUBIC_METERS
-            if data.volume_unit == IquaSoftenerVolumeUnit.LITERS
-            else UnitOfVolume.GALLONS
-        )
+    @property
+    def native_value(self):
+        value = self.coordinator.data.average_daily_use
+        return value / 1000 if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else value
+
+    @property
+    def native_unit_of_measurement(self):
+        return UnitOfVolume.CUBIC_METERS if self.coordinator.data.volume_unit == IquaSoftenerVolumeUnit.LITERS else UnitOfVolume.GALLONS
